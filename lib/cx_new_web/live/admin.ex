f357f9f7c84aa4a025d03747617755e80d04a203
Aksel Stadler <aksel@stadler.no>
2021-12-27 16:20:30 +0100

    add warning two places

--- lib/cx_new_web/live/admin.ex
+++ lib/cx_new_web/live/admin.ex
@@ -143,6 +143,16 @@ defmodule CxNewWeb.AdminLive do
     			<%= if @command do %>
     				<input type="checkbox" checked={@modal_open} class="modal-toggle" >
             <div class="modal">
+                <%= if @alert_content do %>
+                <div class="alert alert-error">
+                  <div class="flex-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-6 h-6 mx-2 stroke-current">    
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"></path>                      
+                    </svg> 
+                    <label><%= @alert_content %></label>
+                  </div>
+                </div>
+                <% end %>
               <div class="modal-box">
               <h3 class="text-lg font-bold mb-2"> <%= CxNew.Helpers.module_to_string(@command) %> </h3>
               <form phx-submit="dispatch" >
