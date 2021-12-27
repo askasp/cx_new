7d664386834b46a1dfe230ab60cec2da4cb81f02
Aksel Stadler <aksel@stadler.no>
2021-12-27 19:56:37 +0100

    sam

--- lib/macros/read_model.ex
+++ lib/macros/read_model.ex
@@ -33,6 +33,8 @@ defmodule ReadModel do
         end
       end
 
+      def get_all(), do: :ets.tab2list(__MODULE__)
+
       def get_bookmark(stream_name) do
         IO.puts "getting bookmark from stream_name"
         IO.inspect stream_name
